Budfix: Entropy sign corrected
Entropy by definition has a - sign in its formula, and I think we need to subtract the entropy from the loss if we use a gradient descent optimiser and want to maximize the entropy (to encourage exploration)

Scripts/a3c_carracing-v0.py
@@ -593,7 +593,7 @@ class Worker:
         # Calculate policy loss
         actions_one_hot = tf.one_hot(memory.actions, self.action_size, dtype=tf.float32)
         policy = tf.nn.softmax(logits)
-        entropy = tf.reduce_sum(policy * tf.log(policy + 1e-20), axis=1)
+        entropy = -tf.reduce_sum(policy * tf.log(policy + 1e-20), axis=1)
         policy_loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=actions_one_hot, logits=logits)
         policy_loss *= tf.stop_gradient(advantage)
         policy_loss -= 0.01 * entropy